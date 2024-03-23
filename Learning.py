5373aa2fc3488a4f3fd55b9312126ec0b707fb03
Sand Rays <naufality@gmail.com>
2024-03-23 12:48:20 +0700

    why use a?

--- Learning.py
+++ Learning.py
@@ -1,7 +1,9 @@
 def shet():
     Money=int(input("How much money?"))
-    if Money >= 200:
+    if Money >= 500:
         print ("damn you rich")
+    elif Money >= 200:
+        print ("okay")
     else:
         print ("poor")
 
